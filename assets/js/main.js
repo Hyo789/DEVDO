16ed5bb527971fac393f43c6d063996503f6b5c7
hyo789 <minorumineta789@gmail.com>
2022-05-07 17:45:18 +0200

    MAJ page d'accueil

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,19 @@
 // LANDING FUNCTION
 
 window.addEventListener("load", function (event) {
+
+    const landing = this.document.querySelector("#landing");
+    if (landing === null || undefined) {
+        return false
+    }
     
     this.setTimeout(function () {
-        document.querySelector("#container_loader").classList.add("disactive");
+        const loader = document.querySelector("#container_loader").classList.add("disactive");
         
         this.setTimeout(function () {
-            document.querySelector("#container_loader").style.display= "none";
+            loader.style.display = "none";
         },2000)
+
     },500);
     
     // .......... ACCUEIL ILLUSTRATION .......... //
@@ -102,4 +108,129 @@ window.addEventListener("load", function (event) {
 
     })();
 
-})
+}) 
+
+// ACCUEIL FUNCTION
+
+// BTN NEXT PREVIOUS
+
+const defaultSection = document.querySelector("#default-section");
+const container = document.querySelector("#container-sections");
+
+// BTN NEXT
+
+const next = document.querySelector("#btn-suivant");
+
+next.addEventListener("click", (event) => {
+    
+    const sectionActive = container.querySelector(".section_active");
+    let nextSection = sectionActive.nextElementSibling;
+    
+    if (defaultSection.classList.contains("section_active") == true) {
+        
+        bullysContainer.querySelector(".default_bully").classList.add("bully-active");
+        
+        defaultSection.classList.remove("section_active");
+        defaultSection.nextElementSibling.classList.add("section_active");
+        
+        setTimeout(() => {
+            defaultSection.style.display = "none";
+        }, 1000)
+        
+    } else if (nextSection == null || undefined) {
+        nextSection = container.firstElementChild.nextElementSibling;
+        sectionActive.classList.remove("section_active");
+        nextSection.classList.add("section_active");
+
+        let bullyActive = bullysContainer.querySelector(".bully-active");
+        bullyActive.classList.remove("bully-active");
+        bullysContainer.querySelector(".default_bully").classList.add("bully-active");
+
+    } else {
+        sectionActive.classList.remove("section_active");
+        nextSection.classList.add("section_active");
+        
+        let bullyActive = bullysContainer.querySelector(".bully-active");
+
+        bullyActive.classList.remove("bully-active");
+        bullyActive.nextElementSibling.classList.add("bully-active");
+        
+        console.log(bullyActive);
+    };
+});
+
+// BTN PREVIOUS
+
+const previous = document.querySelector("#btn-precedant");
+
+previous.addEventListener("click", (event) => {
+
+    const sectionActive = container.querySelector(".section_active");
+    let previousSection = sectionActive.previousElementSibling;
+    
+    if (defaultSection.classList.contains("section_active") == true) {
+
+        bullysContainer.lastElementChild.classList.add("bully-active");
+
+
+        defaultSection.classList.remove("section_active");
+        container.lastElementChild.classList.add("section_active");
+
+        setTimeout(() => {
+            defaultSection.style.display = "none";
+        }, 1000)
+
+    } else if (previousSection == null || undefined || previousSection == defaultSection) {
+        previousSection = container.lastElementChild;
+        sectionActive.classList.remove("section_active");
+        previousSection.classList.add("section_active");
+        
+        let bullyActive = bullysContainer.querySelector(".bully-active");
+        bullyActive.classList.remove("bully-active");
+        bullysContainer.lastElementChild.classList.add("bully-active");
+
+    } else {
+        sectionActive.classList.remove("section_active");
+        previousSection.classList.add("section_active");
+        
+        let bullyActive = bullysContainer.querySelector(".bully-active");
+
+        bullyActive.classList.remove("bully-active");
+        bullyActive.previousElementSibling.classList.add("bully-active");
+    }
+    
+});
+
+// BULLY
+
+const bullysContainer = document.querySelector("#accueil-nav .accueil_nav_list");
+const bullys = document.querySelectorAll("#accueil-nav .accueil_nav_list .accueil_nav_li ")
+
+for (let i = 0; i < bullys.length; i++) {
+
+    let bully = bullys[i];
+
+    bully.addEventListener("click", function (event) {
+        
+        if (bully.classList.contains("bully-active")) {
+            return false
+        } else {
+            let b = bullysContainer.querySelector(".bully-active");
+            
+            if (b == null) {
+                this.classList.add("bully-active");
+            } else {
+                b.classList.remove("bully-active");
+                this.classList.add("bully-active");
+            }
+
+            let h = this.querySelector("a").getAttribute("href");
+            let sectionActive = container.querySelector(".section_active");
+            
+            sectionActive.classList.remove("section_active");
+            container.querySelector(h).classList.add("section_active");
+            
+        } 
+        
+    } )
+}
